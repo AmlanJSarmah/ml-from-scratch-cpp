d6e56dcaa1e276b0655d50177017b9c54b845971
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2026-02-18 09:42:11 +0530

    Created Constructor for KMens

--- include/ml/k_means.hpp
+++ include/ml/k_means.hpp
@@ -3,5 +3,11 @@
 #include "ml/model.hpp"
 
 namespace ml {
-class KMeans : public Model {};
+class KMeans : public Model {
+  Eigen::VectorXd centroids;
+
+public:
+  KMeans(Eigen::MatrixXd X_train, Eigen::VectorXd Y_train,
+         Eigen::MatrixXd X_test, Eigen::VectorXd Y_test);
+};
 } // namespace ml
--- src/k_means.cpp
+++ src/k_means.cpp
@@ -1,1 +1,21 @@
 #include "ml/k_means.hpp"
+#include <Eigen/Dense>
+
+namespace ml {
+KMeans::KMeans(Eigen::MatrixXd X_train, Eigen::VectorXd Y_train,
+               Eigen::MatrixXd X_test, Eigen::VectorXd Y_test) {
+  this->X_train = X_train;
+  this->X_test = X_test;
+  this->Y_train = Y_train;
+  this->Y_test = Y_test;
+  // Set up X_train_scaled and Y_train_scaled etc as original X_train and
+  // Y_train etc. We reset it in test_train_split
+  // We use these scaled variables for all computations, and in case they aren't
+  // actually scaled, the values are populated
+  this->is_scaled = false;
+  this->X_train_scaled = X_train;
+  this->Y_train_scaled = Y_train;
+  this->X_test_scaled = X_test;
+  this->Y_test_scaled = Y_test;
+}
+} // namespace ml
